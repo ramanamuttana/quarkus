193837cfa4e3efc7e6e0c44005f1d27cfc665832
ramanamuttana <ramanamuttana@gmail.com>
2022-12-29 21:31:15 +0100

    added the basic GET and POST

---
+++ QuarkusBasic/src/main/java/org/acme/MovieResource.java
@@ -0,0 +1,45 @@
+package org.acme;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/movies")
+public class MovieResource {
+	
+	public static List<String> movies=new ArrayList<>();
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response getMovies() {
+		
+		return Response.ok(movies).build();
+	}
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/size")
+	public Integer countMovies() {
+		
+		return movies.size();
+		
+	}
+	
+	@POST
+	@Produces(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.TEXT_PLAIN)
+	public Response createMovies(String newMovie) {
+		movies.add(newMovie);
+		return Response.ok(movies).build();
+		
+	}
+	
+
+}
