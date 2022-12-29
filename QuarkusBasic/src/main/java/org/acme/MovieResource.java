dd1302a7ddd7993dc010d8414a529da0bf3f27d9
ramanamuttana <ramanamuttana@gmail.com>
2022-12-29 22:14:49 +0100

    added the PUT method , simply to update the list

--- QuarkusBasic/src/main/java/org/acme/MovieResource.java
+++ QuarkusBasic/src/main/java/org/acme/MovieResource.java
@@ -1,13 +1,17 @@
 package org.acme;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;  
 import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -37,6 +41,24 @@ public class MovieResource {
 	@Consumes(MediaType.TEXT_PLAIN)
 	public Response createMovies(String newMovie) {
 		movies.add(newMovie);
+		return Response.ok(movies).build();
+		
+	}
+	
+	
+	@PUT
+	@Path("{movieToUpdate}")
+	@Produces(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.TEXT_PLAIN)
+	public Response updateMovies(@PathParam("movieToUpdate")String movieToUpdate,@QueryParam("movie")String updateMovie) {
+		movies=movies.stream().map(movie-> {
+			if(movie.equals(movieToUpdate)) {
+				return updateMovie;
+			}
+			return movie;
+			
+		}).collect(Collectors.toList());
+		
 		return Response.ok(movies).build();
 		
 	}
